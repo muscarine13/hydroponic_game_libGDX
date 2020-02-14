6d6f28ea05973a67e6f8ac72eafb98974329d6e6
acidmors <zealot66613@gmail.com>
2020-02-14 21:56:43 +0300

    Нажимается даже эквипа, сменена камера на StretchViewport

--- core/src/com/acidcarpet/hydroponist/screen/BoxScreen.java
+++ core/src/com/acidcarpet/hydroponist/screen/BoxScreen.java
@@ -6,11 +6,13 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 public class BoxScreen implements Screen {
 
@@ -20,12 +22,12 @@ public class BoxScreen implements Screen {
 
     Image background;
 
-    ImageButton lamp_empty;
-    ImageButton light_empty;
-    ImageButton fan_empty;
-    ImageButton plant_empty;
-    ImageButton pot_empty;
-    ImageButton compressor_empty;
+    Image lamp_empty;
+    Image light_empty;
+    Image fan_empty;
+    Image plant_empty;
+    Image pot_empty;
+    Image compressor_empty;
 
     ImageButton x3_button;
     ImageButton shop_button;
@@ -39,7 +41,7 @@ public class BoxScreen implements Screen {
 
 
 private void lamp_click(){
-
+    System.out.println("Lamp click!");
 }
     private void plant_click(){
         System.err.println("Plant click!");
@@ -74,20 +76,17 @@ private void lamp_click(){
 
     @Override
     public void show() {
-        stage = new Stage(new ScreenViewport());
+        stage = new Stage(new StretchViewport(1080, 1920));
         atlas = new TextureAtlas(Gdx.files.internal("skin/box/skin/skin.atlas"));
         skin = new Skin(Gdx.files.internal("skin/box/skin/skin.json"), atlas);
         Gdx.input.setInputProcessor(stage);
         background = new Image(atlas.findRegion("background"));
         background.setBounds(0, 0, stage.getWidth(), stage.getHeight());
 
-        Image lamp_empty_image = new Image(atlas.findRegion("lamp_empty"));
-        lamp_empty_image.setName("lamp_image");
-        lamp_empty_image.setFillParent(true);
-        lamp_empty = new ImageButton(new ImageButton.ImageButtonStyle());
-        lamp_empty.addActor(lamp_empty_image);
+
+        lamp_empty = new Image(atlas.findRegion("lamp_empty"));
         lamp_empty.setName("lamp");
-        lamp_empty.setBounds((stage.getWidth()/2)-(lamp_empty_image.getWidth()/2), stage.getHeight()-lamp_empty_image.getHeight(), lamp_empty_image.getWidth(), lamp_empty_image.getHeight());
+        lamp_empty.setBounds((stage.getWidth()/2)-(lamp_empty.getWidth()/2), stage.getHeight()-lamp_empty.getHeight(), lamp_empty.getWidth(), lamp_empty.getHeight());
         lamp_empty.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -95,21 +94,17 @@ private void lamp_click(){
             }
         });
 
-        Image light_empty_image = new Image(atlas.findRegion("light_empty"));
-        light_empty_image.setFillParent(true);
-        light_empty_image.setName("light_image");
-        light_empty = new ImageButton(new ImageButton.ImageButtonStyle());
-        light_empty.addActor(light_empty_image);
+
+        light_empty = new Image(atlas.findRegion("light_empty"));
+
         light_empty.setName("light");
         light_empty.setBounds(0, 0, stage.getWidth(), stage.getHeight());
+        light_empty.setTouchable(Touchable.disabled);
 
-        Image fan_empty_image = new Image(atlas.findRegion("fan_empty"));
-        fan_empty_image.setFillParent(true);
-        fan_empty_image.setName("fan_image");
-        fan_empty = new ImageButton(new ImageButton.ImageButtonStyle());
-        fan_empty.addActor(fan_empty_image);
+        fan_empty = new Image(atlas.findRegion("fan_empty"));
+
         fan_empty.setName("fan");
-        fan_empty.setBounds(15, stage.getHeight()-500, fan_empty_image.getWidth(), fan_empty_image.getHeight());
+        fan_empty.setBounds(15, stage.getHeight()-500, fan_empty.getWidth(), fan_empty.getHeight());
         fan_empty.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -117,14 +112,11 @@ private void lamp_click(){
             }
         });
 
-        Image plant_empty_image = new Image(atlas.findRegion("plant_empty"));
-        //plant_empty_image.setPosition(0, 0);
-        plant_empty_image.setName("plant_image");
-        plant_empty_image.setFillParent(true);
-        plant_empty = new ImageButton(new ImageButton.ImageButtonStyle());
-        plant_empty.addActor(plant_empty_image);
+
+        plant_empty = new Image(atlas.findRegion("plant_empty"));
+
         plant_empty.setName("plant");
-        plant_empty.setBounds(stage.getWidth()/2-plant_empty_image.getWidth()/2, 500, plant_empty_image.getWidth(), plant_empty_image.getHeight());
+        plant_empty.setBounds(stage.getWidth()/2-plant_empty.getWidth()/2, 500, plant_empty.getWidth(), plant_empty.getHeight());
         plant_empty.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -132,13 +124,10 @@ private void lamp_click(){
             }
         });
 
-        Image pot_empty_image = new Image(atlas.findRegion("pot_empty"));
-        pot_empty_image.setFillParent(true);
-        pot_empty_image.setName("pot_image");
-        pot_empty = new ImageButton(new ImageButton.ImageButtonStyle());
-        pot_empty.addActor(pot_empty_image);
+        pot_empty = new Image(atlas.findRegion("pot_empty"));
+
         pot_empty.setName("pot");
-        pot_empty.setBounds(stage.getWidth()/2-(pot_empty_image.getWidth()/2), 0, pot_empty_image.getWidth(), pot_empty_image.getHeight());
+        pot_empty.setBounds(stage.getWidth()/2-(pot_empty.getWidth()/2), 0, plant_empty.getWidth(), pot_empty.getHeight());
         pot_empty.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -146,27 +135,17 @@ private void lamp_click(){
             }
         });
 
-       Image compressor_empty_image = new Image(atlas.findRegion("compressor_empty"));
-       compressor_empty_image.setFillParent(true);
-       compressor_empty_image.setName("compressor_image");
-       compressor_empty = new ImageButton(new ImageButton.ImageButtonStyle());
-       compressor_empty.addActor(compressor_empty_image);
+
+       compressor_empty = new Image(atlas.findRegion("compressor_empty"));
+
        compressor_empty.setName("compressor");
-       compressor_empty.setBounds(stage.getWidth()-(compressor_empty_image.getWidth()), 0, compressor_empty_image.getWidth(), compressor_empty_image.getHeight());
+       compressor_empty.setBounds(stage.getWidth()-(compressor_empty.getWidth()), 0, compressor_empty.getWidth(), compressor_empty.getHeight());
        compressor_empty.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 compressor_click();
             }
         });
-
-
-
-//        ,
-//        ,
-//        ,
-
-
 
        x3_button = new ImageButton(skin, "x3_button");
        x3_button.addListener(new ClickListener() {
@@ -230,9 +209,10 @@ private void lamp_click(){
         stage.addActor(pot_empty);
         stage.addActor(compressor_empty);
 
-
         stage.addActor(light_empty);
         stage.addActor(lamp_empty);
+
+
 
         stage.addActor(x3_button);
         stage.addActor(shop_button);
