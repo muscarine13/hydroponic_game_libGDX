c320c4ce209d7098c26e5ef76f656400be4ec8d1
acidmors <zealot66613@gmail.com>
2020-04-18 12:39:04 +0300

    ***0.0.11.2*** Исправлено неверное отображение кнопок покупки предметов в магазине саженцев

--- core/src/com/acidcarpet/hydroponist/screen/seedling/SeedlingScreen.java
+++ core/src/com/acidcarpet/hydroponist/screen/seedling/SeedlingScreen.java
@@ -233,7 +233,7 @@ public class SeedlingScreen implements Screen {
         TextButton diamond_buy_button = new TextButton(offer.getDiamond_price()+"",diamond_buttonStyle);
         diamond_buy_button.setBounds(1080-15-200, 15, 200, 80);
         diamond_buy_button.setName("diamond_buy_button");
-        if(offer.may_buy_coin()){
+        if(offer.may_buy_diamond()){
             diamond_buy_button.addListener(new ClickListener() {
                 @Override
                 public void clicked(InputEvent event, float x, float y) {
